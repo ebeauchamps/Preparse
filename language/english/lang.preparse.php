46a4cf100c65f3291e033a59f3c6f4873624cee5
Kelsey Martens <kelsey@solspace.com>
2012-10-11 19:55:03 -0500

    Initial commit

---
+++ language/english/lang.preparse.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Solspace - Preparse
+ *
+ * @package 	Solspace:Preparse
+ * @author		Solspace DevTeam
+ * @copyright	Copyright (c) 2008-2012, Solspace, Inc.
+ * @link		http://solspace.com/docs/addon/c/Preparse
+ * @version		2.0.3
+ * @filesource 	./system/expressionengine/third_party/preparse/language/english/
+ */
+
+$lang = $L = array(
+
+/** --------------------------------------------
+/**  Required for Extensions page
+/** --------------------------------------------*/
+
+"preparse_label" =>
+"Preparse",
+
+"preparse_description" =>
+"Preparse EE Tags in Custom Fields",
+
+/** --------------------------------------------
+/**  Settings Form
+/** --------------------------------------------*/
+
+'channels' =>
+"Channels",
+
+'save' =>
+"Save",
+
+'no_weblogs' =>
+"No Weblogs",
+
+'no_channels' =>
+"No Channels",
+
+'no_custom_fields' =>
+"No Custom Fields",
+
+"outgoing_field_id" =>
+"Outgoing Field",
+
+'choose_field' =>
+"Choose Field",
+
+'custom_field' =>
+"Custom Field",
+
+"template_id" =>
+"Template",
+
+"template_group" =>
+"Template Group",
+
+"error_start" =>
+"An error has occurred while attempting to save the preferences:",
+
+"channel_error" =>
+"The channel submitted in the <strong>%ID%</strong> setting no longer exists.",
+
+"weblog_error" =>
+"The weblog submitted in the <strong>%ID%</strong> setting no longer exists.",
+
+"template_group_error" =>
+"The template group submitted in the <strong>%ID%</strong> setting no longer exists.",
+
+"template_error" =>
+"The template submitted in the <strong>%ID%</strong> setting no longer exists.",
+
+"incoming_field_error" =>
+"The incoming field submitted in the <strong>%ID%</strong> setting no longer exists.",
+
+"outgoing_field_error" =>
+"The outgoing field submitted in the <strong>%ID%</strong> setting no longer exists.",
+
+);
+
+/* End of file lang.preparse.php */
+/* Location: ./system/modules/extensions/preparse/language/english/lang.preparse.php */
+
+?>
