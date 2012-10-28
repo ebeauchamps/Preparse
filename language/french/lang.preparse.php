453862937dc00288bf78342ea48eead0333c2a42
Emmanuel Beauchamps <e.bops@hotmail.fr>
2012-10-28 18:07:13 +0100

    French language pack
    Translated into French and saved as UTF8

---
+++ language/french/lang.preparse.php
@@ -0,0 +1,86 @@
+﻿<?php
+
+/**
+ * Solspace - Preparse
+ *
+ * @package 	Solspace:Preparse
+ * @author		Solspace DevTeam
+ * @copyright	Copyright (c) 2008-2012, Solspace, Inc.
+ * @link		http://solspace.com/docs/addon/c/Preparse
+ * @version		2.0.3
+ * @filesource 	./system/expressionengine/third_party/preparse/language/english/
+ */
+
+$lang = $L = array(
+
+/** --------------------------------------------
+/**  Required for Extensions page
+/** --------------------------------------------*/
+
+"preparse_label" =>
+"Preparse, le pré-analyseur",
+
+"preparse_description" =>
+"Pré-analyse les balises EE dans les champs personnalisés",
+
+/** --------------------------------------------
+/**  Settings Form
+/** --------------------------------------------*/
+
+'channels' =>
+"Canaux",
+
+'save' =>
+"Enregistrer",
+
+'no_weblogs' =>
+"Pas de blog",
+
+'no_channels' =>
+"Pas de canal",
+
+'no_custom_fields' =>
+"Pas de champ personnalisé",
+
+"outgoing_field_id" =>
+"Champ sortant",
+
+'choose_field' =>
+"Choisissez le champ",
+
+'custom_field' =>
+"Champ personnalisé",
+
+"template_id" =>
+"Modèle",
+
+"template_group" =>
+"Groupe de modèles",
+
+"error_start" =>
+"Une erreur est survenue lors de l'enregistrement des préférences :",
+
+"channel_error" =>
+"Le canal soumis dans le paramètre <strong>%ID%</strong> n'existe plus.",
+
+"weblog_error" =>
+"Le blog soumis dans le paramètre <strong>%ID%</strong> n'existe plus.",
+
+"template_group_error" =>
+"Le groupe de modèles soumis dans le paramètre <strong>%ID%</strong> n'existe plus.",
+
+"template_error" =>
+"Le modèle soumis dans le paramètre <strong>%ID%</strong> n'existe plus.",
+
+"incoming_field_error" =>
+"Le champ entrant soumis dans le paramètre <strong>%ID%</strong> n'existe plus.",
+
+"outgoing_field_error" =>
+"Le champ sortant soumis dans le paramètre <strong>%ID%</strong> n'existe plus.",
+
+);
+
+/* End of file lang.preparse.php */
+/* Location: ./system/modules/extensions/preparse/language/french/lang.preparse.php */
+
+?>
